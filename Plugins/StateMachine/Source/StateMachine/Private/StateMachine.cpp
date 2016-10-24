45a58ef1a366fab9be7ca193f07f7eaa1109ac0a
DemonAlecks <alex.batista0@gmail.com>
2016-10-24 13:16:37 -0400

    initialized State Machine plugin code into our project.  //Check output log with warning.

---
+++ Plugins/StateMachine/Source/StateMachine/Private/StateMachine.cpp
@@ -0,0 +1,21 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "StateMachinePrivatePCH.h"
+
+#define LOCTEXT_NAMESPACE "FStateMachineModule"
+
+void FStateMachineModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	UE_LOG(LogTemp, Warning, TEXT("State Machine Module loaded."))
+}
+
+void FStateMachineModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FStateMachineModule, StateMachine)
---
+++ Source/Plugins/PluginsGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "PluginsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINS_API APluginsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
